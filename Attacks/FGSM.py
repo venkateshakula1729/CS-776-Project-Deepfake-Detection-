47083dfb3c0aebf746daf503600614518d6b747f
SaiNikhil <133916662+sainikhils02@users.noreply.github.com>
2025-04-20 15:25:15 +0530

    Update FGSM.py

--- Attacks/FGSM.py
+++ Attacks/FGSM.py
@@ -1,1 +1,19 @@
+import torch
+import torch.nn as nn
 
+def fgsm_attack(model, x, y, epsilon, loss_fn=torch.nn.BCELoss()):
+   
+    # Forward pass
+    outputs = model(x)
+    loss = loss_fn(outputs, y)
+    
+    # Zero gradients, backward pass
+    model.zero_grad()
+    loss.backward()
+    
+    # Get sign of gradients
+    gradient_sign = x.grad.data.sign()
+    
+    # Create and return adversarial example
+    x_adv = x + epsilon * gradient_sign
+    return torch.clamp(x_adv, 0, 1)  # Clip to valid pixel range
