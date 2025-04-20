7ad907f7a27c00480364a7a55ed124a830e988e1
SaiNikhil <133916662+sainikhils02@users.noreply.github.com>
2025-04-20 15:30:26 +0530

    Update one_pixel_attack.py

--- Attacks/one_pixel_attack.py
+++ Attacks/one_pixel_attack.py
@@ -1,1 +1,10 @@
-
+# Note: Since we realised that actual one-pixel attack uses differential evolution and implementing it is
+#        computationally expensive and complicated, we sticked to this basic implementaion in some parts of our models
+def one_pixel_attack(images, pixel_count=1):
+    perturbed = images.clone()
+    batch_size, _, h, w = images.shape
+    for i in range(batch_size):
+        for _ in range(pixel_count):
+            x, y = np.random.randint(0, h), np.random.randint(0, w)
+            perturbed[i, :, x, y] = torch.rand(3)
+    return perturbed
